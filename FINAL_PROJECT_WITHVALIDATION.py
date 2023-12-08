c1f77b3672f0f590d3a963d718880b00b9447836
Xenoyle <connorwfloyd@Gmail.com>
2023-12-07 21:24:10 -0500

    code histogram validation

--- FINAL_PROJECT_WITHVALIDATION.py
+++ FINAL_PROJECT_WITHVALIDATION.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -44,21 +45,26 @@ def piechart():
     pielist2 = [0,0]
     pielist3 = [0,0,0,0]
     pielist4 = [0,0]
+    countlist = [0,0,0,0]
     for i in range(0,len(marijuana)):
         if marijuana[i]==1 and cocaine[i]==1 and heroin[i]==1 and meth[i]==1 and ecstasy[i]==1: # uses no drugs or marijuana
             pielist1[0]+=1
             pielist2[0]+=1
             pielist3[0]+=1
+            countlist[0]+=1
         elif marijuana[i]>1 and (cocaine[i]>1 or heroin[i]>1 or meth[i]>1 or ecstasy[i]>1): # use marijuana and illegal drugs
             pielist3[3]+=1
             pielist4[1]+=1
+            countlist[3]+=1
         elif marijuana[i]>1: # only use marijuana
             pielist1[1]+=1
             pielist3[1]+=1
             pielist4[0]+=1
+            countlist[1]+=1
         elif cocaine[i]>1 or heroin[i]>1 or meth[i]>1 or ecstasy[i]>1: # only use illegal drugs(not marijuana)
             pielist2[1]+=1
             pielist3[2]+=1
+            countlist[2]+=1
     pietotal1 = pielist1[0]+pielist1[1]
     pietotal2 = pielist2[0]+pielist2[1]
     pietotal3 = pielist3[0]+pielist3[1]+pielist3[2]+pielist3[3]
@@ -107,6 +113,10 @@ def piechart():
     # show graph
     fig.tight_layout()
     plt.show()
+    print(f'\n\nNo usage of any drug: \t\t\t\t\t\t\t\t\t{countlist[0]} entries or {pielist3[0]}%')
+    print(f'Only uses Marijuana: \t\t\t\t\t\t\t\t\t{countlist[1]} entries or {pielist3[1]}%')
+    print(f'Only uses Illegal Drugs(Cocaine/Meth/Ecstasy/Heroin): \t{countlist[2]} entries or {pielist3[2]}%')
+    print(f'Uses Marijuana and an Illegal Drug: \t\t\t\t\t{countlist[3]} entries or {pielist3[3]}%')
 def barchart():
     # bar chart
     counter_m = 0
@@ -208,7 +218,103 @@ def barchart():
     print('--------------------------------------------------------------------------------------')
     print(f'Percentage of users that use other illegal drugs along with marijuana: {counter_percent}%')
 
-choice_list = ('heatmap', 'bar graph', 'pie chart', 'all')
+def hist_age():
+    tags = ("13","14","15","16","17","18 or older")
+    # insert some newlines in the tags to better fit into the plot
+    a=[0,0,0,0,0,0]
+    b=[0,0,0,0,0,0]
+    total13 = 0
+    total14 = 0
+    total15 = 0
+    total16 = 0
+    total17 = 0
+    total18 = 0
+    for i in range(0,len(age)):
+        if age[i]==2:
+            total13+=1
+            if marijuana[i]>1:
+                a[0]+=1
+            elif cocaine[i]>1 or heroin[i]>1 or meth[i]>1 or ecstasy[i]>1:
+                b[0]+=1
+        elif age[i]==3:
+            total14+=1
+            if marijuana[i]>1:
+                a[1]+=1
+            elif cocaine[i]>1 or heroin[i]>1 or meth[i]>1 or ecstasy[i]>1:
+                b[1]+=1
+        elif age[i]==4:
+            total15+=1
+            if marijuana[i]>1:
+                a[2]+=1
+            elif cocaine[i]>1 or heroin[i]>1 or meth[i]>1 or ecstasy[i]>1:
+                b[2]+=1
+        elif age[i]==5:
+            total16+=1
+            if marijuana[i]>1:
+                a[3]+=1
+            elif cocaine[i]>1 or heroin[i]>1 or meth[i]>1 or ecstasy[i]>1:
+                b[3]+=1
+        elif age[i]==6:
+            total17+=1
+            if marijuana[i]>1:
+                a[4]+=1
+            elif cocaine[i]>1 or heroin[i]>1 or meth[i]>1 or ecstasy[i]>1:
+                b[4]+=1
+        elif age[i]==7:
+            total18+=1
+            if marijuana[i]>1:
+                a[5]+=1
+            elif cocaine[i]>1 or heroin[i]>1 or meth[i]>1 or ecstasy[i]>1:
+                b[5]+=1
+    a[0]/=total13
+    b[0]/=total13
+    a[1]/=total14
+    b[1]/=total14
+    a[2]/=total15
+    b[2]/=total15
+    a[3]/=total16
+    b[3]/=total16
+    a[4]/=total17
+    b[4]/=total17
+    a[5]/=total18
+    b[5]/=total18
+    
+    # create a dataframe
+    df_hist = pd.DataFrame({"Marijuana User": a, "Illegal Drugs User": b}, index=tags)
+    df_hist.plot.bar(rot=0, figsize=(12, 5))
+    plt.tight_layout() # fit labels etc. nicely into the plot
+    plt.show()
+def hist_sex():
+    tags = ("Female","Male")
+    # insert some newlines in the tags to better fit into the plot
+    a=[0,0]
+    b=[0,0]
+    totalfemale = 0
+    totalmale = 0
+    for i in range(0,len(sex)):
+        if sex[i]==1:
+            totalfemale+=1
+            if marijuana[i]>1:
+                a[0]+=1
+            elif cocaine[i]>1 or heroin[i]>1 or meth[i]>1 or ecstasy[i]>1:
+                b[0]+=1
+        elif sex[i]==2:
+            totalmale+=1
+            if marijuana[i]>1:
+                a[1]+=1
+            elif cocaine[i]>1 or heroin[i]>1 or meth[i]>1 or ecstasy[i]>1:
+                b[1]+=1
+    a[0]/=totalfemale
+    b[0]/=totalfemale
+    a[1]/=totalmale
+    b[1]/=totalmale
+    # create a dataframe
+    df_hist = pd.DataFrame({"Marijuana User": a, "Illegal Drugs User": b}, index=tags)
+    df_hist.plot.bar(rot=0, figsize=(12, 5))
+    plt.tight_layout() # fit labels etc. nicely into the plot
+    plt.show()
+
+choice_list = ('heatmap', 'bar graph', 'pie chart', "histogram", 'all', 'exit')
 choice = ''
 
 print('\n\nWelcome to our program that explores the Youth Risk Behavior System\'s '
@@ -222,13 +328,14 @@ while True:
     # Inner while loop to solicit input from the user as to which graph they would like to see
     while True:
         print('\nWhat graph would you like to see?')
-        choice = input('(Please enter Heatmap, Bar Graph, Pie Chart, All): ').lower()
+        print("\n(THE HISTOGRAM PROVIDES A CORRELATION WITH DEMOGRAPHICS)")
+        choice = input('(Please enter Heatmap/Bar Graph/Pie Chart/Histogram/All/Exit): ').lower()
         print('------------------------------------------------------------------------------------------')
         # Validation to ensure the choice selected is an option, and will notify the user if their choice is 
         # not an option.
         if choice not in choice_list:
             print('\n------------------------------------------------------------------------------------------')
-            print(f'\'{choice}\' is not a valid option. Please try again and choose from the 4 options provided.')
+            print(f'\'{choice}\' is not a valid option. Please try again and choose from the 5 options provided.')
             print('------------------------------------------------------------------------------------------')
         else:
             break
@@ -240,10 +347,15 @@ while True:
         piechart() 
     elif choice == 'heatmap':
         heatmap()
+    elif choice == 'histogram':
+        hist_age()
+        hist_sex()
     elif choice == 'all':
         heatmap()
         piechart()
         barchart()
+    elif choice == 'exit':
+        sys.exit()
     
     # Solicting the user whether to run the loop again to see another graph or stop the program.
     print('Would you like to continue and see another graph?')
